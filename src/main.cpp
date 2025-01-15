a85ff22c405f0cd1c898136918fd888fb612e6e8
Landon Lamoreaux <Ljlamoreaux15@gmail.com>
2025-01-14 18:29:13 -0800

    Initial commit. Program connects to wifi and displays on screen when it is connected.

---
+++ src/main.cpp
@@ -0,0 +1,203 @@
+#include <Arduino.h>
+#include <ESP_Panel_Library.h>
+#include <ESP_IOExpander_Library.h>
+#include <lvgl.h>
+#include <examples/lv_examples.h>
+#include "../lv_conf.h"
+#include <WiFiMulti.h>
+#include <SinricPro.h>
+#include <EEPROM.h>
+
+#define WIFI_SSID "TDS0277"
+#define WIFI_PASS "bgqp25thht"
+
+ESP_Panel *panel = NULL;
+SemaphoreHandle_t lvgl_mux = NULL;
+
+WiFiMulti wifiMulti;
+
+// Extend IO Pin define
+#define TP_RST 1
+#define LCD_BL 2
+#define LCD_RST 3
+#define SD_CS 4
+#define USB_SEL 5
+
+// I2C Pin define
+#define I2C_MASTER_NUM 0
+#define I2C_MASTER_SDA_IO 8
+#define I2C_MASTER_SCL_IO 9
+
+/* LVGL porting configurations */
+#define LVGL_TICK_PERIOD_MS     (2)
+#define LVGL_TASK_MAX_DELAY_MS  (500)
+#define LVGL_TASK_MIN_DELAY_MS  (1)
+#define LVGL_TASK_STACK_SIZE    (4 * 1024)
+#define LVGL_TASK_PRIORITY      (2)
+#define LVGL_BUF_SIZE           (ESP_PANEL_LCD_H_RES * 20)
+
+/* Display flushing */
+void lvgl_port_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+    panel->getLcd()->drawBitmap(area->x1, area->y1, area->x2 + 1, area->y2 + 1, color_p);
+    lv_disp_flush_ready(disp);
+}
+
+void lvgl_port_lock(int timeout_ms)
+{
+    const TickType_t timeout_ticks = (timeout_ms < 0) ? portMAX_DELAY : pdMS_TO_TICKS(timeout_ms);
+    xSemaphoreTakeRecursive(lvgl_mux, timeout_ticks);
+}
+
+void lvgl_port_unlock(void)
+{
+    xSemaphoreGiveRecursive(lvgl_mux);
+}
+
+void lvgl_port_task(void *arg)
+{
+    Serial.println("Starting LVGL task");
+
+    uint32_t task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
+    while (1) {
+        // Lock the mutex due to the LVGL APIs are not thread-safe
+        lvgl_port_lock(-1);
+        task_delay_ms = lv_timer_handler();
+        // Release the mutex
+        lvgl_port_unlock();
+        if (task_delay_ms > LVGL_TASK_MAX_DELAY_MS) {
+            task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
+        } else if (task_delay_ms < LVGL_TASK_MIN_DELAY_MS) {
+            task_delay_ms = LVGL_TASK_MIN_DELAY_MS;
+        }
+        vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
+    }
+}
+
+void connect_to_wifi()
+{
+    wifiMulti.addAP(WIFI_SSID, WIFI_PASS);
+
+    while (wifiMulti.run() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.println("Connecting to Wifi.");
+    }
+    WiFi.setHostname("Reminder Screen.");
+    Serial.println("Connected to Wifi");
+    Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+    Serial.begin(115200); /* prepare for possible serial debug */
+    Serial.println("Hello Arduino!");
+    Serial.println("I am ESP32_Display_Panel.");
+
+    panel = new ESP_Panel();
+
+    Serial.println("Initialize panel");
+    /* Initialize bus and device of panel */
+    panel->init();
+
+    // Initializing the lvgl library.
+    lv_init();
+
+     /* Initialize LVGL buffers */
+    static lv_disp_draw_buf_t draw_buf;
+    /* Using double buffers is more faster than single buffer */
+    /* Using internal SRAM is more fast than PSRAM (Note: Memory allocated using `malloc` may be located in PSRAM.) */
+    uint8_t *buf = (uint8_t *)heap_caps_calloc(1, LVGL_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_INTERNAL);
+    assert(buf);
+    lv_disp_draw_buf_init(&draw_buf, buf, NULL, LVGL_BUF_SIZE);
+
+    /* Initialize the display device */
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = ESP_PANEL_LCD_H_RES;
+    disp_drv.ver_res = ESP_PANEL_LCD_V_RES;
+    disp_drv.flush_cb = lvgl_port_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize IO expander */
+    // ESP_IOExpander *expander = new ESP_IOExpander_CH422G(I2C_MASTER_NUM, ESP_IO_EXPANDER_I2C_CH422G_ADDRESS_000, I2C_MASTER_SCL_IO, I2C_MASTER_SDA_IO);
+    ESP_IOExpander *expander = new ESP_IOExpander_CH422G(I2C_MASTER_NUM, ESP_IO_EXPANDER_I2C_CH422G_ADDRESS_000);
+    expander->init();
+    expander->begin();
+    expander->multiPinMode(TP_RST | LCD_BL | LCD_RST | SD_CS | USB_SEL, OUTPUT);
+    expander->multiDigitalWrite(TP_RST | LCD_BL | LCD_RST | SD_CS, HIGH);
+
+    // Turn off backlight
+    // expander->digitalWrite(USB_SEL, LOW);
+    expander->digitalWrite(USB_SEL, LOW);
+    /* Add into panel */
+    panel->addIOExpander(expander);
+
+    /* Start panel */
+    panel->begin();
+
+    /* Create a task to run the LVGL task periodically */
+    lvgl_mux = xSemaphoreCreateRecursiveMutex();
+    xTaskCreate(lvgl_port_task, "lvgl", LVGL_TASK_STACK_SIZE, NULL, LVGL_TASK_PRIORITY, NULL);
+
+    /* Lock the mutex due to the LVGL APIs are not thread-safe */
+    lvgl_port_lock(-1);
+
+    /*Change the active screen's background color*/
+    Serial.println("Creating Text");
+
+    // Creating a style.
+    static lv_style_t style;
+    lv_style_init(&style);
+    lv_style_set_radius(&style, 5);
+
+    /*Make a gradient*/
+    lv_style_set_width(&style, 150);
+    lv_style_set_height(&style, LV_SIZE_CONTENT);
+
+    lv_style_set_pad_ver(&style, 20);
+    lv_style_set_pad_left(&style, 5);
+
+    lv_style_set_x(&style, lv_pct(50));
+    lv_style_set_y(&style, 80);
+
+    /*Create an object with the new style*/
+    lv_obj_t * obj = lv_obj_create(lv_scr_act());
+    lv_obj_add_style(obj, &style, 0);
+
+    lv_obj_t * label = lv_label_create(obj);
+    lv_obj_set_pos(obj, ESP_PANEL_LCD_H_RES/2 - 350, ESP_PANEL_LCD_V_RES/2 - 100);
+    lv_obj_set_size(obj, 700, 200);
+
+    lv_label_set_text(label, "Connecting to Wifi");
+    // lv_obj_set_style_text_font(label, &arial70px, 0)
+    lv_obj_center(label);
+
+    connect_to_wifi();
+
+    lv_label_set_text(label, "Connected to Wifi");
+
+    /* Release the mutex */
+    lvgl_port_unlock();
+
+    // Serial.println("Draw color bar from top to bottom, the order is B - G - R");
+    // uint16_t line_per_bar = ESP_PANEL_LCD_V_RES / ESP_PANEL_LCD_COLOR_BITS;
+    // uint16_t *color = (uint16_t *)calloc(1, line_per_bar * ESP_PANEL_LCD_H_RES * ESP_PANEL_LCD_COLOR_BITS / 8);
+    // for (int j = 0; j < ESP_PANEL_LCD_COLOR_BITS; j++) {
+    //     for (int i = 0; i < line_per_bar * ESP_PANEL_LCD_H_RES; i++) {
+    //         color[i] = 1ULL << j;
+    //     }
+    //     panel->getLcd()->drawBitmap(0, j * line_per_bar, ESP_PANEL_LCD_H_RES, (j + 1) * line_per_bar, color);
+    // }
+    // free(color);
+
+    Serial.println("Setup done");
+}
+
+void loop()
+{
+    Serial.println("Loop");
+    sleep(1);
+}
